efb1a97343b5edbe4a5d04c2bd2751934ff5acad
Wh04m7 <slipknot.rulit3@gmail.com>
2025-02-20 01:24:23 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
-    const swiper = new Swiper('.swiper', {
+const toursSwiper = new Swiper('.tours__swiper', {
         // Опции Swiper
         slidesPerView: 4.7, // Количество слайдов, отображаемых одновременно
         spaceBetween: 32, // Расстояние между слайдами
-        
+        loop: true,
         scrollbar: {
             el: '.swiper-scrollbar',
             draggable: true,
