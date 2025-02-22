b3aa1c435003ceb4cbf8694f9efd6e25017f74fa
Wh04m7 <slipknot.rulit3@gmail.com>
2025-02-22 18:00:57 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
 
-    const toursSwiper = new Swiper('.tours__slider', {
-        // Опции Swiper
-        slidesPerView: 4, // Количество слайдов, отображаемых одновременно
-        spaceBetween: 32,
+const toursSwiper = new Swiper('.tours__slider', {
+    slidesPerView: 4,
+    spaceBetween: 32,
         
-        scrollbar: {
-            el: '.tours__scrollbar',
-            draggable: true,
-        },
-    });
+    scrollbar: {
+        el: '.tours__scrollbar',
+        draggable: true,
+    },
+});
 
 
-    const hotelsSwiper = new Swiper('.hotels__slider', {
-        // Опции Swiper
-        slidesPerView: 3, // Количество слайдов, отображаемых одновременно
-        spaceBetween: 60,
-        centeredSlides: true,
-        loop: true,
+const hotelsSwiper = new Swiper('.hotels__slider', {
+    slidesPerView: 3,
+    spaceBetween: 60,
+    centeredSlides: true,
+    loop: true,
 
-        scrollbar: {
-            el: '.hotels__scrollbar',
-            draggable: true,
-        },
-    });
+    scrollbar: {
+        el: '.hotels__scrollbar',
+        draggable: true,
+    },
+});
 
-    const reviewsSwiper = new Swiper('.reviews__slider', {
-        // Опции Swiper
-        slidesPerView: 3, // Количество слайдов, отображаемых одновременно
-        spaceBetween: 32,
-        loop: true,
 
-        scrollbar: {
-            el: '.reviews__scrollbar',
-            draggable: true,
-        },
-    });
+const reviewsSwiper = new Swiper('.reviews__slider', {
+    slidesPerView: 3, 
+    spaceBetween: 32,
+        oop: true,
+
+    scrollbar: {
+        el: '.reviews__scrollbar',
+        draggable: true,
+    },
+});
 
