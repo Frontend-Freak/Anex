1504fb9ddbfb8d22086f808204c2652cafa5dec6
Wh04m7 <slipknot.rulit3@gmail.com>
2025-02-23 01:56:25 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//СЛФЙДЕР НОМЕР 1
 const toursSwiper = new Swiper('.tours__slider', {
     slidesPerView: 4,
     spaceBetween: 32,
@@ -9,7 +9,7 @@ const toursSwiper = new Swiper('.tours__slider', {
     },
 });
 
-
+//СЛФЙДЕР НОМЕР 2
 const hotelsSwiper = new Swiper('.hotels__slider', {
     slidesPerView: 3,
     spaceBetween: 60,
@@ -22,7 +22,7 @@ const hotelsSwiper = new Swiper('.hotels__slider', {
     },
 });
 
-
+//СЛФЙДЕР НОМЕР 1
 const reviewsSwiper = new Swiper('.reviews__slider', {
     slidesPerView: 3, 
     spaceBetween: 32,
@@ -34,7 +34,7 @@ const reviewsSwiper = new Swiper('.reviews__slider', {
     },
 });
 
-
+//БУРГЕР МЕНЮ
 document.getElementById('burger__button').addEventListener('click', function() {
     const menu = document.getElementById('burger__menu');
     menu.classList.toggle('show'); 
@@ -44,3 +44,36 @@ document.getElementById('burger__button').addEventListener('click', function() {
     lines[2].classList.toggle('line3');
 });
 
+
+//МОДАЛКА
+const btn = document.querySelector('.header__btn')
+const modal = document.querySelector('.modal')
+const body = document.body
+
+
+const openModal = () => {
+    modal.classList.add('modal--open')
+    body.classList.add('body--fixed')
+}
+
+
+const closeModal = () => {
+    modal.classList.remove('modal--open')
+    body.classList.remove('body--fixed')
+}
+
+btn.addEventListener('click', openModal)
+
+modal.addEventListener('click', event =>{
+    const target = event.target
+
+    if(target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')){
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if(event.code === 'Escape' && modal.classList.contains('modal--open')){
+        closeModal()
+    }
+})
